c3b99009d17eb932fb78f1b67686df0dcc948596
kalkidan12 <kalget12@gmail.com>
2022-08-29 20:08:19 +0300

    ui

--- client/src/pages/LaptopPage.jsx
+++ client/src/pages/LaptopPage.jsx
@@ -1,8 +1,251 @@
 import React from "react";
 import "./pages.css";
+import "./pages.css";
+import { IoMdAddCircle } from "react-icons/io";
+import { MdFavorite } from "react-icons/md";
+import laptop from "../images/laptop1.png";
 
 function LaptopPage() {
-	return <div>LaptopPage</div>;
+	return (
+		<div>
+			<div className="home-title">
+				<h1></h1>
+				<h2>List of trending products available in our atore</h2>
+			</div>
+			<div className="trending-product-list">
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="trending-card">
+					<div className="card-content">
+						<div className="for-image">
+							<div className="favorite">
+								<span>
+									<MdFavorite />
+								</span>
+							</div>
+							<div className="img">
+								<img src={laptop} alt="" />
+							</div>
+						</div>
+						<div className="for-title">
+							<h4>Lenevo T440</h4>
+						</div>
+						<div className="for-price">
+							<h2>$1900</h2>
+							<span>
+								<IoMdAddCircle />
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LaptopPage;
